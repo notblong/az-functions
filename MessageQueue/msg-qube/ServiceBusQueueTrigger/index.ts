0716150394709513f967401584d5211384272418
Pham Bao Long <pblo@netcompany.com>
2023-03-29 15:02:00 +0700

    QueueTriggerFunction: support query user by uuid in CosmosDB then create *.docx and stored in Blob Storage

--- MessageQueue/msg-qube/ServiceBusQueueTrigger/index.ts
+++ MessageQueue/msg-qube/ServiceBusQueueTrigger/index.ts
@@ -1,7 +1,43 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions";
+import { Document, Paragraph, TextRun, Packer } from 'docx';
 
-const serviceBusQueueTrigger: AzureFunction = async function(context: Context, mySbMsg: any): Promise<void> {
-    context.log('ServiceBus queue trigger function processed message', mySbMsg);
+const serviceBusQueueTrigger: AzureFunction = async function (context: Context, mySbMsg: any, users: any): Promise<void> {
+    context.log('ServiceBus queue triggered', mySbMsg);
+    context.log('UserData retrieve from CosmosDB', JSON.stringify(users));
+
+    if (!users) {
+        // Not found.
+        return;
+    }
+
+    // create docx
+    context.log(`Create ${mySbMsg.uuid}.docx....`);
+    const doc = new Document({
+        sections: [
+            {
+                properties: {},
+                children: [
+                    new Paragraph({
+                        children: [
+                            new TextRun({
+                                text: `Hello ${users[0].name}`,
+                                bold: true,
+                            }),
+                            new TextRun(`email: ${users[0].email}`),
+                            new TextRun(`user: ${users[0].username}`),
+                            new TextRun({
+                                text: "\tThis document is powered by DocxJS",
+                                bold: true,
+                            }),
+                        ],
+                    }),
+                ],
+            },
+        ],
+    });
+
+    const buffer = await Packer.toBuffer(doc);
+    context.bindings.myOutputBlob = buffer;
 };
 
 export default serviceBusQueueTrigger;
