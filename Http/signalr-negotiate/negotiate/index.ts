38a2b0bbdcffab0e5590d8e7ab37629b962e50fa
Pham Bao Long <pblo@netcompany.com>
2023-04-03 14:15:26 +0700

    added hand-shake SignalR endpoint

--- Http/signalr-negotiate/negotiate/index.ts
+++ Http/signalr-negotiate/negotiate/index.ts
@@ -2,17 +2,18 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, signalRConnectionInfo: any): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.log(signalRConnectionInfo);
+    context.log('signalRConnectionInfo:', signalRConnectionInfo);
     context.res = {
-        status: 200, /* Defaults to 200 */
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Request-Headers': 'X-Custom-Header'
+        },
         body: signalRConnectionInfo
     };
-
 };
 
 export default httpTrigger;
